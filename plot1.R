01a75ff647770e73ac1a818072d1b782bcb7882a
Clayton Smith <cssmithy@gmail.com>
2014-10-11 18:19:18 +1100

    initial plot

---
+++ plot1.R
@@ -0,0 +1,25 @@
+#skipping rows requires naming columns.
+pwr <- read.csv("household_power_consumption.txt", header=F,
+        sep=";", skip=(1440*46), nrows=(1440*3), na.strings="?",
+        col.names = c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+        colClasses = c(rep("character",2),rep("numeric",7)) )
+
+#clean up, subset
+pwr <- subset(pwr, pwr$Date == "1/2/2007" | pwr$Date == "2/2/2007")
+row.names(pwr) <- seq(nrow(pwr))
+
+#convert chr to Date, POSIXct
+pwr$Time <- strptime(paste(pwr$Date,pwr$Time), "%d/%m/%Y %H:%M:%S")
+
+#Date not actually reqd
+pwr$Date <- NULL
+names(pwr)[1] <- "datetime"
+
+#plots
+
+hist(pwr$Global_active_power, col="red",
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)")
+
+#copy to images
+#"plot1.png"
